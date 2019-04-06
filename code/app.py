5c8698ff1b49b354845b7d1045b19ed720c348e1
Nzei95 <nzeimark@gmail.com>
2019-04-06 15:55:20 +0100

    Add reqparse for parsing arguments and requirements.txt file

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -12,7 +12,15 @@ jwt = JWT(app, authenticate, identity) # creates a new endpoint /auth
 
 items = []
 
+
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+            type=float,
+            required=True,
+            help="This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         '''
@@ -35,7 +43,7 @@ class Item(Resource):
         '''
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with that name '{}' already exists.".format(name)}, 400
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price' : data['price']}
         items.append(item)
         return item, 201
@@ -44,6 +52,16 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message' : 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
